656eddef0b046806cc2df825f494ce76dac0b4e9
Muhammad Numan <muhammadnuman2004@gmail.com>
2025-09-07 00:42:26 -0400

    lab1 completed

--- src/app.cpp
+++ src/app.cpp
@@ -1,18 +1,42 @@
-// practicing some imperative C++
-// see README for instructions
 #include <iostream>
-#include <string>
 using namespace std;
 
-bool isPrime(int x);
-int closestPrime(int x);
-/** You should put in a main for testing, but then comment it out for submission.
- * The test.cpp file will include the main in your submission. You do not edit the test.cpp 
- * file. The only file to edit is app.cpp**/
-/**** 
+bool isPrime(int x) {
+    if (x <= 1) return false;
+    for (int i = 2; i < x; i++) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int closestPrime(int x) {
+    if (isPrime(x)) {
+        return x;
+    }
+    return closestPrime(x+1);
+}
+/****
+
 int main()
-{
-    
+{  
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (isPrime(a)) {
+        cout << a << " is prime. ";
+    } else {
+        cout << a << " is not prime. Closest prime is " << closestPrime(a) << ". ";
+    }
+
+    if (isPrime(b)) {
+        cout << b << " is prime.";
+    } else {
+        cout << b << " is not prime. Closest prime is " << closestPrime(b) << ".";
+    }
+
     return 0;          
 }
 ****/
